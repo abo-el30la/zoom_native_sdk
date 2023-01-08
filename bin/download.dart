8160b5c9d0ff50fefc72571764726bdbf06d2a61
abo-el30la <112876065+abo-el30la@users.noreply.github.com>
2023-01-08 17:32:45 +0200

    fix:(ios mobile rtc)

--- bin/download.dart
+++ bin/download.dart
@@ -42,7 +42,7 @@ void main(List<String> args) async {
 }
 
 Future<void> checkAndDownloadSDK(String location) async {
-  var iosSDKFile = '$location/ios/MobileRTC.xcframework/ios-arm64/MobileRTC.framework/MobileRTC';
+  var iosSDKFile = '$location/ios/MobileRTC.xcframework/ios-arm64_armv7/MobileRTC.framework/MobileRTC';
   bool exists = await File(iosSDKFile).exists();
 
   // if (!exists) {
